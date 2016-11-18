7416a1454796a85ddd1e57ae9c5a1d4bc3d363b1
mosclofri <mt.tayfur@gmail.com>
2016-11-19 02:09:10 +0300

    Fix locating apk for windows

--- testapi/src/main/java/com/appium/api/server/AppiumServer.java
+++ testapi/src/main/java/com/appium/api/server/AppiumServer.java
@@ -7,8 +7,11 @@ import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 public class AppiumServer {
 
@@ -28,7 +31,13 @@ public class AppiumServer {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, System.getProperty("platform.name"));
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, System.getProperty("device.name"));
         capabilities.setCapability(MobileCapabilityType.NO_RESET, Boolean.valueOf(System.getProperty("no.reset")));
-        capabilities.setCapability(MobileCapabilityType.APP, new File(ClassLoader.getSystemResource(System.getProperty("file")).getFile()).getAbsolutePath());
+        try {
+            capabilities.setCapability(MobileCapabilityType.APP,
+                    new File(URLDecoder.decode(ClassLoader.getSystemResource(System.getProperty("file")).getFile(),
+                            StandardCharsets.UTF_8.toString())).getAbsolutePath());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         return capabilities;
     }
 
@@ -41,4 +50,5 @@ public class AppiumServer {
             this.service.stop();
         }
     }
+
 }

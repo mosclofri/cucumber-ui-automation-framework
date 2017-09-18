a83855200cffb93bdaf7bd135aa522e933a81754
mosclofri <mt.tayfur@gmail.com>
2017-09-19 00:17:00 +0300

    Improve DataFactory

--- java-support-framework/src/main/java/com/support/framework/support/DataFactory.java
+++ java-support-framework/src/main/java/com/support/framework/support/DataFactory.java
@@ -1,12 +1,56 @@
 package com.support.framework.support;
 
 import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.DateProducer;
+import io.codearte.jfairy.producer.company.Company;
+import io.codearte.jfairy.producer.payment.CreditCard;
 import io.codearte.jfairy.producer.person.Person;
+import io.codearte.jfairy.producer.text.TextProducer;
 
-public class DataFactory {
+public final class DataFactory {
 
     private static final String CURRENT_PERSON = "CURRENT_PERSON";
+    private static final String CURRENT_COMPANY = "CURRENT_COMPANY";
+    private static final String CURRENT_TEXT_PRODUCER = "CURRENT_TEXT_PRODUCER";
+    private static final String CURRENT_DATE_PRODUCER = "CURRENT_DATE_PRODUCER";
+    private static final String CURRENT_CREDIT_CARD = "CURRENT_CREDIT_CARD";
     private static Fairy fairy = Fairy.create();
+
+    public static Company getCompany() {
+        Company company = fairy.company();
+        setCurrentCompany(company);
+        return company;
+    }
+
+    public static CreditCard getCreditCard() {
+        CreditCard creditCard = fairy.creditCard();
+        setCurrentCreditCard(creditCard);
+        return creditCard;
+    }
+
+    public static Company getCurrentCompany() {
+        return ThreadLocalMap.getItem(CURRENT_COMPANY, Company.class);
+    }
+
+    public static void setCurrentCompany(Company company) {
+        ThreadLocalMap.getMap().put(CURRENT_COMPANY, company);
+    }
+
+    public static CreditCard getCurrentCreditCard() {
+        return ThreadLocalMap.getItem(CURRENT_CREDIT_CARD, CreditCard.class);
+    }
+
+    public static void setCurrentCreditCard(CreditCard creditCard) {
+        ThreadLocalMap.getMap().put(CURRENT_CREDIT_CARD, creditCard);
+    }
+
+    public static DateProducer getCurrentDateProducer() {
+        return ThreadLocalMap.getItem(CURRENT_DATE_PRODUCER, DateProducer.class);
+    }
+
+    public static void setCurrentDateProducer(DateProducer dateProducer) {
+        ThreadLocalMap.getMap().put(CURRENT_DATE_PRODUCER, dateProducer);
+    }
 
     public static Person getCurrentPerson() {
         return ThreadLocalMap.getItem(CURRENT_PERSON, Person.class);
@@ -16,34 +60,30 @@ public class DataFactory {
         ThreadLocalMap.getMap().put(CURRENT_PERSON, person);
     }
 
+    public static TextProducer getCurrentTextProducer() {
+        return ThreadLocalMap.getItem(CURRENT_TEXT_PRODUCER, TextProducer.class);
+    }
+
+    public static void setCurrentTextProducer(TextProducer textProducer) {
+        ThreadLocalMap.getMap().put(CURRENT_TEXT_PRODUCER, textProducer);
+    }
+
+    public static DateProducer getDateProducer() {
+        DateProducer dateProducer = fairy.dateProducer();
+        setCurrentDateProducer(dateProducer);
+        return dateProducer;
+    }
+
     public static Person getPerson() {
         Person person = fairy.person();
         setCurrentPerson(person);
         return person;
     }
 
-    public static String getRandomEmail() {
-        return fairy.person().getEmail();
-    }
-
-    public static String getRandomFullName() {
-        return fairy.person().getFullName();
-    }
-
-    public static String getRandomLastName() {
-        return fairy.person().getLastName();
-    }
-
-    public static String getRandomName() {
-        return fairy.person().getFirstName();
-    }
-
-    public static String getRandomParagraph(int sentenceCount) {
-        return fairy.textProducer().paragraph(sentenceCount);
-    }
-
-    public static String getRandomSentence(int worldCount) {
-        return fairy.textProducer().sentence(worldCount);
+    public static TextProducer getTextProducer() {
+        TextProducer textProducer = fairy.textProducer();
+        setCurrentTextProducer(textProducer);
+        return textProducer;
     }
 }
 
